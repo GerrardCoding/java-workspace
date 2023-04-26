3d6b0b2c03e2b771ce2b60b26ce182a3f3573e0a
DKoding <foejakzm@naver.com>
2023-04-26 10:03:39 +0900

    0426

--- 04_MVC_JDBC_PreparedStatement/src/com/goodee/run/Run.java
+++ 04_MVC_JDBC_PreparedStatement/src/com/goodee/run/Run.java
@@ -1,5 +1,23 @@
 package com.goodee.run;
 
+import com.goodee.view.MainMenu;
+
 public class Run {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * MVC모델 : 프로그램 디자인패턴 중 하나. model1, model2가 있음.
+		 * 								mode12(MVC 각 기능을 분리)가 있음.
+		 * 
+		 * M(Model) : 데이터 처리 담당.(데이터들을 담기 위한 클래스(vo), DB에 직접 접근해서 데이터를 처리하는 클래스(dao)
+		 * V(View)  : 화면을 담당.(사용자가 보게되는 시각적인 요소, 출력 및 입력)
+		 * C(Controller) : 사용자의 요청을 처리를 담당.
+		 * 
+		 */
+		
+		
+		new MainMenu().mainMenu();
+	}
 
 }
